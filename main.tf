f869b321bd3dab56c92854f829000ebd12999fd7
chetanraval07 <chetanravalcr03@gmail.com>
2026-01-12 18:36:50 +0530

    hello

--- main.tf
+++ main.tf
@@ -9,10 +9,20 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
 #################################
+
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-bucxx"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
 
 
